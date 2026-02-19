c141722cf5c5f670acce6b65802096cc95918e10
syhros <79529557+syhros@users.noreply.github.com>
2026-02-19 09:28:45 +0000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,5 +12,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-  }
+  },
+  optimizeDeps: {
+    exclude: ['yahoo-finance2', '@vercel/node'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['yahoo-finance2', '@vercel/node'],
+    },
+  },
 })
